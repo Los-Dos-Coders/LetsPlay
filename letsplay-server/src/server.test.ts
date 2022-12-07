e869bc59fc471581afe6d52008f92f1293e14491
Shawn Long <shawnlong636@gmail.com>
2022-12-07 13:41:25 -0800

    added jest and supertest testing to backend

---
+++ letsplay-server/src/server.test.ts
@@ -0,0 +1,14 @@
+import app from './app';
+
+const mockListen = jest.fn();
+app.listen = mockListen;
+
+afterEach(() => {
+  mockListen.mockReset();
+});
+
+test('Server Works', async () => {
+  require('./server');
+  expect(mockListen.mock.calls.length).toBe(1);
+  expect(mockListen.mock.calls[0][0]).toBe(process.env.PORT || 3000);
+});
