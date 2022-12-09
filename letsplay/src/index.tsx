3248de8024e094707727d65681d64f53336d6540
Shawn Long <shawnlong636@gmail.com>
2022-12-08 19:26:06 -0800

    Fixed unused import in frontend

--- letsplay/src/index.tsx
+++ letsplay/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import startMirage from './mirage';
 import GameList from './GameList';
