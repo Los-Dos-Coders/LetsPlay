34bec6e7ea5a01a4e6d2a3abe91e49c832529171
Shawn Long <shawnlong636@gmail.com>
2022-12-14 19:21:47 -0800

    Removed --test-cert from certbot commands

--- letsplay/nginx/renewCertificate.sh
+++ letsplay/nginx/renewCertificate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-m=$(/usr/local/bin/certbot renew --test-cert && /usr/sbin/nginx -s reload 2>&1)
+m=$(/usr/local/bin/certbot renew && /usr/sbin/nginx -s reload 2>&1)
 
 curl -fsS --retry 5 --data-raw "$m" https://hc-ping.com/0e697fb4-177c-4ff5-a30c-e3af404d4fbe/$?
