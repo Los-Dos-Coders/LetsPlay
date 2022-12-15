c3761a92ced15ffc44739c11edbaaffedcf33419
Shawn Long <shawnlong636@gmail.com>
2022-12-14 19:47:19 -0800

    Removed commented out code from rebase

--- letsplay/nginx/GetCertificates.sh
+++ letsplay/nginx/GetCertificates.sh
@@ -3,20 +3,6 @@
 set -e
 
 requestFromCertbot() {
-  
-  # TODO: MODIFY CERTBOT -> INSTALL ONTO NGINX CONTAINER
-  # TODO: SETUP AUTORENEW USING CRONTAB
-
-  ## SAMPLE SCRIPT TO RENEW
-  # # add crontab entry to renew the letsencrypt certificate
-  # echo "adding crontab"
-  # echo "0 23 * * * certbot renew --dry-run" | crontab -
-
-  # # start nginx
-  # echo "starting nginx"
-  # nginx
-
-  ## END SAMPLE SCRIPT
 
   # Get Credentials
   touch /ssl/certbot-creds.ini
